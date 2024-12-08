40e952becbe27bbb7a777f1a7bd7fde54986c6d9
Paolo de Malmazet <paolodmc8@gmail.com>
2024-12-08 21:05:36 +0100

    modified:   database.js 	renamed:    images/asus-prime-z490-a-03-620163479-removebg-preview.png -> images/asus-prime-z490-a.png 	renamed:    images/61CRh5dUkcL-4242713100-removebg-preview.png -> images/rog-maximus-z790-hero.png 	renamed:    images/13-132-954-V01-3211368901-removebg-preview.png -> images/rog-strix-z270-i-gaming.png 	renamed:    images/thermalright-tl-c12cw-s-x3-white.png -> images/thermalright-tl-c12cw-s-white.png 	modified:   index.html

--- database.js
+++ database.js
@@ -22,12 +22,15 @@ function createDatabase() {
             { type: 'case', name: 'microATX Deepcool Matrexx 40', image: 'deepcool-matrexx-40-3fs.png'},
             { type: 'case', name: 'ATX Deepcool CH560 Digital', image: 'deepcool-ch560-digital.png'},
             { type: 'case', name: 'ATX Corsair ICUE 5000X White', image: 'corsair-icue-5000x-white.png'},
-            { type: 'case', name: 'ATX Corsair ICUE 5000X White', image: 'corsair-icue-5000x-white.png'},
             { type: 'case', name: 'ATX Cooler-Master Masterbox NR200P White', image: 'coolermaster-masterbox-nr200p-white.png'},
             { type: 'gpu', value: 'gigabyte-aero-4080', name: 'Gigabyte Aero 4080', image: 'gigabyte-4080-aero.png'},
             { type: 'gpu', value:'msi-gaming-x-slim-xt-4060-ti', name: 'MSI Gaming-X Slim 4060-ti', image: 'msi-4060-ti-gaming-x-slim.png'},
             { type: 'gpu', value:'asus-rog-strix-4080', name: 'Asus ROG STRIX 4080', image: 'rog-4080-strix.png'},
             { type: 'gpu', value:'asus-tuf-4080', name: 'Asus TUF 4080', image: 'tuf-4080.png'},
+            { type: 'gpu', value:'asus-tuf-rx-7900-xt', name: 'Asus TUF RX 7900XT', image: 'tuf-rx-7900-xt.png'},
+            { type: 'ram', value:'teamgroup-t-force-delta-rgb-2x8gb-ddr5', name: 'Teamgroup T Force Delta RGB 16GB (2x8gb) DDR5', image: 'teamgroup-t-force-delta-rgb-ddr5.png'},
+            { type: 'ram', value:'teamgroup-t-force-delta-rgb-2x16gb-ddr5', name: 'Teamgroup T Force Delta RGB 32GB (2x16gb) DDR5', image: 'teamgroup-t-force-delta-rgb-ddr5.png'},
+            { type: 'ram', value:'teamgroup-t-force-delta-rgb-2x16gb-ddr5', name: 'Teamgroup T Force Delta RGB 64GB (2x32gb) DDR5', image: 'teamgroup-t-force-delta-rgb-ddr5.png'},
         ];
 
         // Prépare l'insertion des components
